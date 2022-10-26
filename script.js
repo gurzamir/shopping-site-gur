b5336d48e3a907be5177b0d122fadb4b556c0c10
gurzamir <gurzamir88@gmail.com>
2022-10-26 20:47:20 +0300

    arrays of products

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const productContainer=document.getElementById("products-containers");
+
+
+const productList = ["black Tea", "Green Tea"]
 const blackTea=document.createElement("div");
 const greenTea=document.createElement("div");
 
-blackTea.innerHTML = "Black Tea";
-greenTea.innerHTML = "Green Tea";
+blackTea.innerHTML = productList[0];
+greenTea.innerHTML = productList[1];
 
 blackTea.classList.add("product");
 greenTea.classList.add("product");
 
 productContainer.appendChild(blackTea);
 productContainer.appendChild(greenTea);
+
