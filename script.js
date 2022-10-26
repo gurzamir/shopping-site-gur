81362f964d65c1a0f9eb707e7d408ab275da8e75
gurzamir <gurzamir88@gmail.com>
2022-10-26 20:00:33 +0300

    added dynemic html in js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const productContainer=document.getElementById("products-containers");
+const blackTea=document.createElement("div");
+const greenTea=document.createElement("div");
+
+blackTea.innerHTML = "Black Tea";
+greenTea.innerHTML = "Green Tea";
+
+blackTea.classList.add("product");
+greenTea.classList.add("product");
+
+productContainer.appendChild(blackTea);
+productContainer.appendChild(greenTea);
