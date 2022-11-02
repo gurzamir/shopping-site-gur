0ca47b0801ecc77c9fae33ffba777241946a0ae7
gurzamir <gurzamir88@gmail.com>
2022-11-02 17:19:13 +0200

    loop try 1 after save

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const productContainer=document.getElementById("products-containers");
+console.log('hello')
+const productContainer=document.getElementById("products-conteiner");
+/*const product1 = document.createElement('div')
+product1.innerHTML = 'green tea'
+product1.classList.add('product');
+productContainer.appendChild(product1);*/
+const productArray = ['green tea', 'chai', 'coffee', 'icecream', 'soup', 'chocko', 'coconut', 'lior-fav'];
+
+for (let i = 0; i < productArray.length; i++ ){
+    console.log(productArray.length);
+    console.log(productArray[i]);
+    const product1 = document.createElement('div');
+    product1.classList.add('product')
+    product1.innerHTML = productArray[i]
+    console.log(product1.innerHTML);
+    productContainer.appendChild(product1)
 
 
-let productList = ["black Tea", "Green Tea","coffee", "chai", "black coffee", "house mix"]
-for (let i = 0; i<2; i++) {
-    if (i = 0){
-    const product1 = document.createElement("div");
-   product1.classList.add("product");
-   product1.innerHTML = productList[0];
-   productContainer.appendChild(product1);
-   
-   }else if (i > 0 && i < 2){
-        const product2 = document.createElement("div");
-        product2.classList.add("product");
-        productContainer.appendChild(product2);
-        product2.innerHTML = productList[1];
-    }else if (i>2){
-    const product3 = document.createElement("div");
-    product3.classList.add("product");
-    productContainer.appendChild(product3);
-    product3.innerHTML = productList[2];
-   }
-}
+}    
+
+
+
 // const blackTea=document.createElement("div");
 // const greenTea=document.createElement("div");
 
@@ -32,4 +31,3 @@ for (let i = 0; i<2; i++) {
 
 // productContainer.appendChild(blackTea);
 // productContainer.appendChild(greenTea);
-
