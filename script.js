7bc453fba78957b4f7ad5b375f591edbffbaa818
gurzamir <gurzamir88@gmail.com>
2022-11-09 17:49:21 +0200

    array and loops

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 console.log('hello')
-const productContainer=document.getElementById("products-conteiner");
+const productContainer=document.getElementById("products-containers");
 /*const product1 = document.createElement('div')
 product1.innerHTML = 'green tea'
 product1.classList.add('product');
 productContainer.appendChild(product1);*/
-const productArray = ['green tea', 'chai', 'coffee', 'icecream', 'soup', 'chocko', 'coconut', 'lior-fav'];
+const productArray = [{
+    name :'green tea',
+    price : 12,
+    id : 0,
+    amount : 1,
+    descreption : 'healthy and refreshing'
+    }
+    
+    ,{
+    name : 'chai',
+    price : 12,
+    id : 1,
+    amount : 1,
+    descreption : ' aromtic flavour for cold nights' 
+    }   
+]
+
+    
+    // 'coffee', 'icecream', 'soup', 'chocko', 'coconut', 'lior-fav'];
+    function test1(obj){
+        console.log(1)
+        const product = document.createElement('div');
+        product.innerHTML = obj.name;
+        productContainer.appendChild(product);
+        product.classList.add('product');
+        const productPrice = document.createElement('div');
+        productPrice.innerHTML = obj.price;
+        product.appendChild(productPrice) 
+        const productDescreption = document.createElement('div');
+        productDescreption.innerHTML = obj.descreption;
+        product.appendChild(productDescreption);
+    }
 
 for (let i = 0; i < productArray.length; i++ ){
-    console.log(productArray.length);
-    console.log(productArray[i]);
+     test1(productArray[i]);
+    // console.log(productArray[i].name);
+    // const product =  document.createElement('div');
+    // product.innerHTML = productArray[i].name;
+    // console.log(product.innerHTML)
+    // const productPrice = document.createElement('div');
+    // productPrice.innerHTML = productArray[i].price;
+    // productContainer.appendChild(product)
+    // product.classList.add('product');
+    // product.appendChild(productPrice)
+
+
+}
+
+
+
+    /*
     const product1 = document.createElement('div');
     product1.classList.add('product')
     product1.innerHTML = productArray[i]
     console.log(product1.innerHTML);
-    productContainer.appendChild(product1)
-
-
-}    
-
+    productContainer.appendChild(product1);
+}   
+*/
 
 
 // const blackTea=document.createElement("div");
-// const greenTea=document.createElement("div");
+// const greenTea=document.createElement(div");
 
 // blackTea.innerHTML = productList[0];
 // greenTea.innerHTML = productList[1];
