498bc147a0a51b994836d7d1d9c9d5c0104068af
gurzamir <gurzamir88@gmail.com>
2022-11-16 10:53:27 +0200

    added images to objects in the array

--- script.js
+++ script.js
@@ -4,74 +4,92 @@ const productContainer=document.getElementById("products-containers");
 product1.innerHTML = 'green tea'
 product1.classList.add('product');
 productContainer.appendChild(product1);*/
-const productArray = [{
-    name :'green tea',
+const productArray = []
+productArray.push(
+    {
+        name :'green tea',
+        price : 12,
+        id : 0,
+        amount : 1,
+        descreption : 'healthy and refreshing',
+        image : 'images/green tea.jpg',
+   },
+       
+  {
+        name : 'chai',
+        price : 12,
+        id : 1,
+        amount : 1,
+        descreption : ' aromtic flavour for cold nights',
+        image : 'images/chai.jpeg'
+        
+   },
+   {
+            name : 'milk',
+            price : 5,
+            id : 2,
+            amount : 1,
+            descreption : 'wow!',
+            image :'images/milk.jpg',
+   },
+   {
+    name : 'fruit-shake',
+    price : 6,
+    id : 3,
+    amount : 5,
+    descreption : 'mixed seasons fruits with vanilla milk',
+    image : 'images/fruit.jpg',
+   },
+   {
+    name : 'black tae',
+    price : 10,
+    id : 4,
+    amount : 6,
+    descreption : 'aromatic flavour',
+    image : 'images/black tea.jpg' ,
+   },
+   {
+    name : 'coffee',
     price : 12,
-    id : 0,
-    amount : 1,
-    descreption : 'healthy and refreshing'
-    }
+    id : 5,
+    amount : 10,
+    descreption : 'boost of energy',
+    image : 'images/coffee.jpeg',
+   }
     
-    ,{
-    name : 'chai',
-    price : 12,
-    id : 1,
-    amount : 1,
-    descreption : ' aromtic flavour for cold nights' 
-    }   
-]
+   
+)
 
     
     // 'coffee', 'icecream', 'soup', 'chocko', 'coconut', 'lior-fav'];
     function test1(obj){
         console.log(1)
         const product = document.createElement('div');
-        product.innerHTML = obj.name;
-        productContainer.appendChild(product);
-        product.classList.add('product');
         const productPrice = document.createElement('div');
-        productPrice.innerHTML = "price is: " + obj.price;
-        product.appendChild(productPrice) 
         const productDescreption = document.createElement('div');
-        productDescreption.innerHTML = obj.descreption;
+        const productAmount = document.createElement('div');
+        const productimg = document.createElement('img');
+        
+        product.classList.add('product');
+        productimg.classList.add('images')
+
+        product.innerHTML = obj.name;
+        productPrice.innerHTML = "price of " + obj.name + " is: " + obj.price;
+        productDescreption.innerHTML ='about ' + obj.name +': ' + obj.descreption;
+        productAmount.innerHTML ='anount of ' + obj.name + 'is: ' + obj.amount;
+        productimg.src = obj.image;
+        
+        product.appendChild(productimg);
+        productContainer.appendChild(product);
+        product.appendChild(productPrice);
         product.appendChild(productDescreption);
+        product.appendChild(productAmount);
+
+        // productimg = obj.image.src;
     }
 
 for (let i = 0; i < productArray.length; i++ ){
      test1(productArray[i]);
-    // console.log(productArray[i].name);
-    // const product =  document.createElement('div');
-    // product.innerHTML = productArray[i].name;
-    // console.log(product.innerHTML)
-    // const productPrice = document.createElement('div');
-    // productPrice.innerHTML = productArray[i].price;
-    // productContainer.appendChild(product)
-    // product.classList.add('product');
-    // product.appendChild(productPrice)
-
 
 }
 
-
-
-    /*
-    const product1 = document.createElement('div');
-    product1.classList.add('product')
-    product1.innerHTML = productArray[i]
-    console.log(product1.innerHTML);
-    productContainer.appendChild(product1);
-}   
-*/
-
-
-// const blackTea=document.createElement("div");
-// const greenTea=document.createElement(div");
-
-// blackTea.innerHTML = productList[0];
-// greenTea.innerHTML = productList[1];
-
-// blackTea.classList.add("product");
-// greenTea.classList.add("product");
-
-// productContainer.appendChild(blackTea);
-// productContainer.appendChild(greenTea);
